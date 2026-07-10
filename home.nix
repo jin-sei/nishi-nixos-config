a6f0f3f8bb6d2413d46f3186ecfb551dea30ee87
Jin Sei <nils.rivaillon@protonmail.com>
2026-07-10 16:48:51 +0200

    added some stuff in my hyprland config

--- home.nix
+++ home.nix
@@ -36,6 +36,24 @@ hl.monitor({
 
 local terminal = "alacritty"
 
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+    misc = {
+        force_default_wallpaper = 0,
+        disable_hyprland_logo   = true,
+    },
+})
+
 ---------------------
 ---- KEYBINDINGS ----
 ---------------------
@@ -44,6 +62,28 @@ local mainMod = "SUPER"
 
 hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + Q", hl.dsp.window.close())
+
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+    input = {
+        kb_layout  = "fr",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
 
 	'';
 };
