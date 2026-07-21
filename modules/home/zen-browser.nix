9a3c6b68a482e9f8ae90eda34e9815df4a8e2c46
Jin Sei <nils.rivaillon@protonmail.com>
2026-07-21 17:09:05 +0200

    adde some zen browser settings

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -7,8 +7,54 @@
 	];
 
 	programs.zen-browser = {
+		
 		enable = true;
 		setAsDefaultBrowser = true;
 		enablePrivateDesktopEntry = true;
+		
+		policies.Cookies.Allow = [
+			"https://github.com"
+			"https://moodle.utc.fr"
+			"https://utc.fr"
+			"https://account.proton.me"
+			"https://app.todoist.com"
+			"https://mail.google.com"
+			"https://calendar.proton.me"
+			"https://google.com"
+			"https://letterboxd.com"
+			"https://serializd.com"
+			"https://duckduckgo.com"
+			"https://webmail.utc.fr"
+			"https://drive.google.com"
+			"https://ent.utc.fr"
+			"https://duck.ai"
+		];
+		
+		profiles.default = {
+			search = {
+				force = true;
+				default = "ddg";
+			};
+			settings = {
+				"privacy.sanitize.sanitizeOnShutdown" = true;
+				"privacy.clearOnShutdown.cache" = true;
+				"privacy.clearOnShutdown.cookies" = true;
+				"privacy.clearOnShutdown.downloads" = true;
+				"privacy.clearOnShutdown.formdata" = true;
+				"privacy.clearOnShutdown.history" = true;
+				"privacy.clearOnShutdown.offlineApps" = true;
+				"privacy.clearOnShutdown.sessions" = true;
+				"privacy.clearOnShutdown.siteSettings" = false;
+				
+				"network.cookie.lifetimePolicy" = 2;
+				
+				"full-screen-api.ignore-widgets" = true;
+
+				"browser.translations.enable" = false;
+				"browser.translations.automaticallyPopup" = false;
+				"extensions.formautofill.addresses.enabled" = false;
+				"extensions.formautofill.creditCards.enabled" = false;
+			};
+		};
 	};
 }
