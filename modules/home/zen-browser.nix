0f24bf000b1693bea46afcdd45c7fd0715e8426c
Jin Sei <nils.rivaillon@protonmail.com>
2026-07-22 13:14:11 +0200

    added some custom search engines to zen

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -34,6 +34,27 @@
 			search = {
 				force = true;
 				default = "ddg";
+				engines = {
+					nix-packages = {
+    						name = "Nix Packages";
+						urls = [{
+							template = "https://search.nixos.org/packages";
+							params = [
+								{ name = "type"; value = "packages"; }
+								{ name = "query"; value = "{searchTerms}"; }
+							];
+						}];
+						
+						icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+						definedAliases = [ "@nix" ];
+					};
+
+					youtube = {
+						name = "YouTube";
+						urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
+						definedAliases = [ "@you" "@yt" ];
+					};	
+				};
 			};
 			settings = {
 				"privacy.sanitize.sanitizeOnShutdown" = true;
