69946ccfcb959b98d66bf259f79e1930d78db3be
Jin Sei <nils.rivaillon@protonmail.com>
2026-09-04 14:10:04 +0900

    modified starship prompt

--- common/home/shell.nix
+++ common/home/shell.nix
@@ -32,26 +32,26 @@
 		enableZshIntegration = true;
 		settings = {
 
-			format = "$username$hostname$directory$git_branch$git_status$line_break$character";
+			format = "╭─$username$hostname$directory$git_branch$git_status$line_break╰─$character";
 			add_newline = true;
 
 			character = {
-				success_symbol = "[\\$>](bold green)";
-				error_symbol = "[\\$>](bold red)"; 
+				success_symbol = "──[>](green)";
+				error_symbol = "──[>](red)"; 
 			};
 
 			username = {
 				show_always = true;
-				format = "[$user]($style)";
+				format = "[$user](bold cyan)";
 			};
 			
 			hostname = {
 				ssh_only = false;
-				format = "@[$hostname]($style) "; 
+				format = "@[$hostname](bold purple) "; 
 			};
 			
 			directory = {
-				format = "[$path]($style)";
+				format = "[$path](bold cyan)";
 				truncation_length = 1;
       				truncation_symbol = "";
       				truncate_to_repo = false;
@@ -60,7 +60,7 @@
 			
 			git_branch = {
 				symbol = ""; 
-				format = ":[$branch]($style) ";
+				format = ":[$branch](bold blue) ";
 			};
 			
 			git_status = {
