8521ebd6e9a240d6c807aeaed96248ff3d02ee23
Jin Sei <nils.rivaillon@protonmail.com>
2026-09-07 16:01:00 +0900

    greyed out comments for neovim (in colors.lua)

--- miscellaneous/dotfiles/neovim/lua/colors.lua
+++ miscellaneous/dotfiles/neovim/lua/colors.lua
@@ -8,5 +8,9 @@ vim.cmd("colorscheme default")
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "NONE", bg = "NONE" })
 
+-- grey comments
+vim.api.nvim_set_hl(0, "Comment", { ctermfg = 8, fg = "#555555", italic = true })
+vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
+
 -- grey line numbers
 vim.api.nvim_set_hl(0, 'LineNr', { ctermfg = 8, fg = '#555555' })
