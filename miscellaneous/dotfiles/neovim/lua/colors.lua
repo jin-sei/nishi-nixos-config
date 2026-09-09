751186091482a2b2ac414c85e5168908486f7139
Jin Sei <nils.rivaillon@protonmail.com>
2026-09-09 18:19:47 +0900

    change in neovim theme

--- miscellaneous/dotfiles/neovim/lua/colors.lua
+++ miscellaneous/dotfiles/neovim/lua/colors.lua
@@ -1,5 +1,5 @@
 -- turn off true colors to fall back to ANSI palette
-vim.opt.termguicolors = false
+vim.opt.termguicolors = true
 
 -- keep default colorscheme (terminal)
 vim.cmd("colorscheme default")
@@ -8,9 +8,9 @@ vim.cmd("colorscheme default")
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "NONE", bg = "NONE" })
 
--- grey comments
-vim.api.nvim_set_hl(0, "Comment", { ctermfg = 8, fg = "#555555", italic = true })
-vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
-
 -- grey line numbers
 vim.api.nvim_set_hl(0, 'LineNr', { ctermfg = 8, fg = '#555555' })
+
+-- grey comments (following line color number)
+vim.api.nvim_set_hl(0, "Comment", { link = "LineNr", italic = true })
+vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
