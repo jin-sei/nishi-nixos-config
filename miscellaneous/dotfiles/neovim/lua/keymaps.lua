00a1ad82f23860940b7aac980fb18bd62004d757
Jin Sei <nils.rivaillon@protonmail.com>
2026-09-09 10:54:01 +0900

    commented out swap between ; and :

--- miscellaneous/dotfiles/neovim/lua/keymaps.lua
+++ miscellaneous/dotfiles/neovim/lua/keymaps.lua
@@ -1,3 +1,7 @@
 -- use 'jk' or 'kj' simultaneously to exit insert mode
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
 vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })
+
+-- swap ":" and ";" (for us keybaord)
+-- vim.keymap.set({ "n", "v" }, ";", ":", { noremap = true })
+-- vim.keymap.set({ "n", "v" }, ":", ";", { noremap = true })
