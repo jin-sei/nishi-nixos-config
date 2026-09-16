ff54aac89e523b0ca66765235aa659404fc1b5d3
Jin Sei <nils.rivaillon@protonmail.com>
2026-09-16 21:39:50 +0900

    wireplumber stuff for waybar againnnnn

--- common/home/desktop-environment.nix
+++ common/home/desktop-environment.nix
@@ -222,6 +222,7 @@ in
 				pulseaudio = {
 					format = "<span color='${waybarEtiquetteColor}'>音量:</span>{volume:02}%";
 					format-bluetooth = "<span color='${waybarEtiquetteColor}'>無線:</span>{volume:02}%";
+					format-bluetooth-muted = "<span color='${waybarEtiquetteColor}'>無線:</span>{volume:02}%";
 					tooltip = false;
 					on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
 					on-click-right = "alacritty --class WaybarSpawned -T PipeWire -e wiremix";
